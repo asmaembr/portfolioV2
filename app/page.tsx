ceef0da52eb20a7ec05b8a30d576d4b873e0cdf4
asmaembr <asmae.moubr@gmail.com>
2024-03-28 21:47:17 +0000

    public pics

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-      home
+    <main className="w-screen h-screen relative">
+      <div className="flex items-center w-full h-full bg-cover bg-center" style={{backgroundImage: "url(/)"}}>
+        <div className="pl-20 md:pl-40 pb-56 md:pb-20 flex flex-col gap-5 z-[10] max-w-[750px]">
+          <h1 className="text-[50px] text-white"></h1>
+          </div>
+      </div>
     </main>
   );
 }
