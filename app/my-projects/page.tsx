a17b125c244358e7f237b92762420778fedb3421
asmaembr <asmae.moubr@gmail.com>
2024-03-30 20:03:38 +0000

    slider done

--- app/my-projects/page.tsx
+++ app/my-projects/page.tsx
@@ -1,7 +1,20 @@
 "use client"
 import React from 'react';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import { Projects } from '@/constants';
+import Image from 'next/image';
+import Link from 'next/link';
 
 const Page = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1
+  };
   return (
     <div className='w-screen h-screen overflow-hidden'>
       <video
@@ -13,7 +26,40 @@ const Page = () => {
       </video>
 
       <div className='relative z-10 w-full h-full flex items-center justify-center'>
-        <h1 className='text-white text-4xl font-bold'>Your Content Here</h1>
+      <div className='w-3/4 m-auto'>
+      <div className="mt-20">
+      <Slider {...settings}>
+        {Projects.map((d) => (
+          <div key={d.title} className="bg-[#] h-[450px] text-black rounded-xl">
+            <div className='h-56 flex justify-center items-center'>
+              <Image
+               src={d.src}
+                alt={d.title} 
+                width={d.width}
+                height={d.height}
+                />
+            </div>
+
+            <div className="flex flex-col items-center justify-center gap-4 p-4 ">
+              <p className="text-xl font-semibold">{d.title}</p>
+              <p className="text-center">{d.text}</p>
+              <Link 
+              key={d.title}
+              href={d.link}
+              className=' text-white text-lg px-6 py-1 rounded-xl'>
+                  <Image
+                  src='/github.svg'
+                  alt=""
+                  width={30}
+                  height={30}/>
+              </Link>
+            </div>
+          </div>
+        ))}
+      </Slider>
+      </div>
+      
+    </div>
       </div>
     </div>
   );
--- constants/index.ts
+++ constants/index.ts
@@ -2,12 +2,6 @@ import { RxHome, RxPerson, RxDashboard, RxClipboard, RxActivityLog, RxAccessibil
 
 export const SkillData = [
   {
-    name: "Java",
-    Image: "/java.png",
-    width: 100,
-    height: 100,
-  },
-  {
     name: "C++",
     Image: "/cpp.png",
     width:80,
@@ -19,18 +13,11 @@ export const SkillData = [
     width:60,
     height: 60,
   },
-
   {
-    name: "Git ",
-    Image: "/git.png",
-    width: 80,
-    height: 80,
-  },
-  {
-    name: "Flutter",
-    Image: "/flutter.png",
-    width: 80,
-    height: 80,
+    name: "Java",
+    Image: "/java.png",
+    width: 90,
+    height: 90,
   },
   {
     name: "Spring",
@@ -39,26 +26,20 @@ export const SkillData = [
     height: 80,
   },
   {
-    name: "Python",
-    Image: "/python.png",
+    name: "Git ",
+    Image: "/git.png",
     width: 80,
     height: 80,
-  },
-  {
-    name: "MySQL DB",
-    Image: "/mysql.png",
-    width: 80,
-    height: 80,
-  },
-  {
-    name: "Oracle Database",
-    Image: "/oracle.png",
-    width: 100,
-    height: 100,
   },
   {
     name: "Docker",
     Image: "/docker.webp",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Flutter",
+    Image: "/flutter.png",
     width: 80,
     height: 80,
   },
@@ -77,8 +58,8 @@ export const SkillData = [
   {
     name: "Css",
     Image: "/css.png",
-    width: 100,
-    height: 100,
+    width: 90,
+    height: 90,
   },
   {
     name: "TypeScript",
@@ -86,6 +67,27 @@ export const SkillData = [
     width: 70,
     height: 70,
   },
+  
+  {
+    name: "Python",
+    Image: "/python.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "MySQL DB",
+    Image: "/mysql.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Oracle Database",
+    Image: "/oracle.png",
+    width: 100,
+    height: 100,
+  },
+  
+  
 ];
 export const Socials = [
 
@@ -111,26 +113,41 @@ export const Socials = [
   }
 ];
 export const Projects = [
+
   {
-    title: "Modern Nextjs Website",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
-    src: "/NextWebsite.png",
+    title:"Course Management ",
+    text: "Creating a WordPress site to handle certificates and courses, with Flutter's mobile UI features like WebView and Network Dependencies integrated.",
+    src: "/mycourse.png",
+    link:"https://github.com/asmaembr/MyCourse-Flutter-App",
+    width: 60,
+    height: 60,
   },
   {
-    title: "Space Themed Website",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
-    src: "/SpaceWebsite.png",
+    title:"ChatBot - Amigo",
+    text: "Creating a mobile application that provides a chatbot service for the users using Flutter and DialogFlow concepts such as DashChat , HTTP requests .",
+    src: "/amigo.png",
+    link:"https://github.com/asmaembr/Amigo-app",
+    width: 60,
+    height:60,
   },
   {
-    title: "Modern Nextjs Portfolio",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
-    src: "/WebPortfolio.png",
+    title:" Flutter Jazz Music ",
+    text: "Building a Jazz music player mobile app with Flutter architecture, Dart models, and utilizing Flutter packages and widgets.",
+    src: "/jazzapp.png",
+    link:"https://github.com/asmaembr/flutter-music-app",
+    width: 60,
+    height:60,
   },
   {
-    title: "Matrix themed Website",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
-    src: "/Matrix.png",
+    title:"Artificial Intelligence Project",
+    text: "Developing a Desktop App for age and gender detection, applying machine learning and model training with Python tools like Tkinter and OpenCV.",
+    src: "/facia.png",
+    link:"https://github.com/asmaembr/Artificial-Intelligence-Project-June-2023-",
+    width: 60,
+    height:60,
   },
+
+
 ];
 export const NavLinks = [
   {
