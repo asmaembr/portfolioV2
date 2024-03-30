32ff269d6fbb53b3b013f52a2f1c578a826b8126
asmaembr <asmae.moubr@gmail.com>
2024-03-30 23:44:32 +0000

    last changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Asmae | Portfolio",
   description: "Asmae Moubarriz's portfolio",
+  icons: ['/mypic.ico']
 };
 
 export default function RootLayout({
